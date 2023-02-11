32bfe0619c1b3498af265173d8c47d4dd37668b1
syr9707 <yrshin9771@gmail.com>
2023-02-11 17:11:06 +0900

    test(User) : 회원가입(inser) 기능 구현 테스트

--- src/test/java/com/testbook/domain/user/UserRepositoryTest.java
+++ src/test/java/com/testbook/domain/user/UserRepositoryTest.java
@@ -1,4 +1,46 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.testbook.domain.user;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
 class UserRepositoryTest {
-  
+
+    @Autowired
+    UserRepository userRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void 회원저장_불러오기() {
+        // given
+        String name = "ummchicken";
+        String email = "aaa@test.com";
+        String password = "12345678";
+
+        userRepository.save(User.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .build());
+
+        // when
+        List<User> userList = userRepository.findAll();
+
+        // then
+        User user = userList.get(0);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getEmail()).isEqualTo(email);
+        assertThat(user.getPassword()).isEqualTo(password);
+    }
+
 }
