916eaba1e39edcb601bb49d3f9de8acc75364245
syr9707 <yrshin9771@gmail.com>
2023-02-11 15:00:02 +0900

    feat(PostsList) : 전체 게시물 조회 DTO 생성!!!
    드디어 DTO의 구조를 세분화하기 시작했다!

--- src/main/java/com/testbook/web/dto/PostsListResponseDto.java
+++ src/main/java/com/testbook/web/dto/PostsListResponseDto.java
@@ -1,2 +1,21 @@
-package com.testbook.web.dto;public class PostsListResponseDto {
+package com.testbook.web.dto;
+
+import com.testbook.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
 }
