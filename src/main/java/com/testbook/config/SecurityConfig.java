e8200d3d55e56f28c62f640fe7b3e833b0fedf8d
syr9707 <yrshin9771@gmail.com>
2023-02-13 11:10:48 +0900

    test(PasswordEncoder) : 비밀번호 암호화 테스트

--- src/main/java/com/testbook/config/SecurityConfig.java
+++ src/main/java/com/testbook/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,27 +16,36 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .loginPage("/users/login")
-                .defaultSuccessUrl("/")
-                .usernameParameter("email")
-                .failureUrl("/members/login/error")
+//        http.formLogin()
+//                .loginPage("/users/login")
+//                .defaultSuccessUrl("/")
+//                .usernameParameter("email")
+//                .failureUrl("/members/login/error")
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+//                .logoutSuccessUrl("/")
+//        ;
+//
+//        http.authorizeRequests()
+//                .mvcMatchers("/css/**", "/js/**", "/img/**").permitAll()
+//                .mvcMatchers("/").permitAll()
+//                .mvcMatchers("/api/v1/**").hasRole("ADMIN")
+//                .anyRequest().authenticated()
+//        ;
+//
+//        http.exceptionHandling()
+//                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+//        ;
+
+        http
+                .httpBasic().disable() // rest api 이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
+                .formLogin().disable()
+                .csrf().disable() // rest api이므로 csrf 보안이 필요없으므로 disable처리.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt token으로 인증하므로 세션은 필요없으므로 생성안함.
                 .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
-                .logoutSuccessUrl("/")
-        ;
-
-        http.authorizeRequests()
-                .mvcMatchers("/css/**", "/js/**", "/img/**").permitAll()
-                .mvcMatchers("/").permitAll()
-                .mvcMatchers("/api/v1/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-        ;
-
-        http.exceptionHandling()
-                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-        ;
+                .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
+                .antMatchers("/**").permitAll();
 
         return http.build();
     }
@@ -43,7 +53,7 @@ public class SecurityConfig {
     // 비밀번호 암호화 저장
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); // 비밀번호를 암호화하기 위한 다양한 알고리즘 중 하나
     }
 
 }
