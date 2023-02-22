0c6f9336acc8e5b5483a0f69d233effa4a56d8a8
syr9707 <yrshin9771@gmail.com>
2023-02-22 22:18:12 +0900

    feat(SecurityConfig) : 기존 코드 수정

--- src/main/java/com/testbook/config/SecurityConfig.java
+++ src/main/java/com/testbook/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.testbook.config;
 
+import com.testbook.domain.user.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,50 +12,39 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.formLogin()
-//                .loginPage("/users/login")
-//                .defaultSuccessUrl("/")
-//                .usernameParameter("email")
-//                .failureUrl("/members/login/error")
-//                .and()
-//                .logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
-//                .logoutSuccessUrl("/")
-//        ;
-//
-//        http.authorizeRequests()
-//                .mvcMatchers("/css/**", "/js/**", "/img/**").permitAll()
-//                .mvcMatchers("/").permitAll()
-//                .mvcMatchers("/api/v1/**").hasRole("ADMIN")
-//                .anyRequest().authenticated()
-//        ;
-//
-//        http.exceptionHandling()
-//                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-//        ;
-
-        http
-                .httpBasic().disable() // rest api 이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
-                .formLogin().disable()
-                .csrf().disable() // rest api이므로 csrf 보안이 필요없으므로 disable처리.
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt token으로 인증하므로 세션은 필요없으므로 생성안함.
-                .and()
-                .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
-                .antMatchers("/**").permitAll();
-
-        return http.build();
-    }
+//    private final CustomOAuth2UserService customOAuth2UserService;
 
     // 비밀번호 암호화 저장
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // 비밀번호를 암호화하기 위한 다양한 알고리즘 중 하나
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint();
+//                .userService(customOAuth2UserService);
+
+        return http.build();
     }
 
 }
